4ea1c70749492a80f889326e1ce25dd34d86de19
Eduardo Rosales Alvarado <edrosrs99@gmail.com>
2025-08-11 17:22:00 -0600

    update

--- factorial.js
+++ factorial.js
@@ -1,33 +1,35 @@
 alert("Hola");
 
 const factorial = (fact) => {
-    // if(fact.trim() === "" || isNaN(fact) || fact === null) {
-    //     alert("Ingrese un valor correcto");
-    //     location.reload();
-    // }
-    if (fact < 0) {
-        return "No se pudo calcular factorial, el valor proporcionado es: " + fact;
-    } 
-    if (fact == 0) {
-        return 1;
-    }
+  if (fact < 0) {
+    return "No se pudo calcular factorial, el valor proporcionado es: " + fact;
+  }
+  if (fact == 0) {
+    return 1;
+  }
 
-    const resultado = 1;
-    for(i = 1; i <= fact; i++) {
-        resultado = resultado + i;
-    }
+  let resultado = 1;
+  for (let i = 1; i <= fact; i++) {
+    resultado = resultado * i;
+  }
 
-    return resultado;
-}
+  return resultado;
+};
 
 const mostrarFactorial = () => {
-    const valor = prompt("Valor para obtener el factorial: ");
-    const numero = parseInt(valor);
-    const valFac = factorial(numero);
-    const h1 = document.getElementsByTagName("h1");
-    console.log(valFac);
-}
+  event.preventDefault();
+  const valor = prompt("Valor para obtener el factorial: ");
+  if (valor === null || valor.trim() === "" || isNaN(valor)) {
+    alert("Valor ingresado no válido, por favor ingresa un número entero.");
+    location.reload();
+  }
+  const numero = parseInt(valor);
+  const valFac = factorial(numero);
+  const elementoh1 = document.getElementById("factorial");
+  console.log(valFac);
+  elementoh1.textContent = `El factorial del número "${numero}" es: ${valFac}`;
+};
 
 window.onload = () => {
-    mostrarFactorial();
-}
+  mostrarFactorial();
+};
